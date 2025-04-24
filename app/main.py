45209712a2fd1052af2fe0567800b5275f0ea01c
matze <104998443+prodmatze@users.noreply.github.com>
2025-04-24 21:45:43 +0300

    can now handle multiple connections concurretnly

--- app/main.py
+++ app/main.py
@@ -15,44 +15,47 @@ def build_response(content):
     ).encode("utf-8")
 
 
+def handle_request(client_socket, client_address):
+    req_msg = client_socket.recv(1024)
+    print(f"Client sent req_msg: {req_msg}")
 
+    url = get_url_from_get_request(req_msg)
+    sub_urls = get_sub_urls(url)
+
+    response = None
+
+    if sub_urls:
+        match sub_urls[0]:
+            case "/" :
+                response = response_200
+            case "echo":
+                response = build_response(sub_urls[1])
+            case "user-agent":
+                user_agent = get_header_value_from_request(req_msg, "User-Agent:")
+                response = build_response(user_agent)
+            case _ :
+                response = response_404
+    
+    elif url == "/":
+        response = response_200
+    else:
+        response = response_404
+
+    client_socket.send(response)
+
+    client_socket.close()
+
+    return None
 
 def main():
-
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
 
     while True:
         client_socket, client_address = server_socket.accept()
         print(f"Incoming connection from client adress: {client_address}")
 
-        req_msg = client_socket.recv(1024)
-        print(f"Client sent req_msg: {req_msg}")
+        handle_request(client_socket, client_address)
 
-        url = get_url_from_get_request(req_msg)
-        sub_urls = get_sub_urls(url)
-
-        response = None
-
-        if sub_urls:
-            match sub_urls[0]:
-                case "/" :
-                    response = response_200
-                case "echo":
-                    response = build_response(sub_urls[1])
-                case "user-agent":
-                    user_agent = get_header_value_from_request(req_msg, "User-Agent:")
-                    response = build_response(user_agent)
-                case _ :
-                    response = response_404
-        
-        elif url == "/":
-            response = response_200
-        else:
-            response = response_404
-
-        get_header_value_from_request(req_msg, "Host:")
-
-        client_socket.send(response)
         
 #GET requests
 def get_url_from_get_request(request):
@@ -74,12 +77,12 @@ def get_sub_urls(url):
 def get_header_value_from_request(request, header_key):
     request_string = request.decode("utf-8")
 
-    request_string_split_0 = request_string.split(header_key)[1].strip()
-    request_string_split_1 = request_string_split_0.split(" ")
-    extracted_info = request_string_split_1[0].strip()
+    for line in request_string.split("\r\n"):
+        if line.startswith(header_key):
+            return line.split(header_key)[1].split()
+        else:
+            return "ERROR"
 
-    print(f"EXTRACTED INFO: {extracted_info}")
-    return extracted_info
 
 if __name__ == "__main__":
     main()
