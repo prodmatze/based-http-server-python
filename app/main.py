70ae3598f914d1afd57afe27743204f6bdd38f57
matze <104998443+prodmatze@users.noreply.github.com>
2025-04-24 15:02:50 +0300

    implemented url check

--- app/main.py
+++ app/main.py
@@ -1,15 +1,47 @@
 import socket  # noqa: F401
 
 
-def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
+response_200 = b"HTTP/1.1 200 OK\r\n\r\n"
+response_400 = b"HTTP/1.1 404 Not found\r\n\r\n"
 
-    # Uncomment this to pass the first stage
-    #
+
+def main():
+
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+
+    client_socket, client_address = server_socket.accept()
+    print(f"Incomming connection from client adress: {client_address}")
+
+    req_msg = client_socket.recv(1024)
+    print(f"Client sent req_msg: {req_msg}")
+
+    url = get_url_from_request(req_msg)
+
+    response = None
+    if url == "/":
+        response = response_200
+    else:
+        response = response_400
+
+    client_socket.send(response)
+        
+
+    
+
+def get_url_from_request(request):
+    request_string = request.decode("utf-8")
+    request_string_split_0 = request_string.split("GET")
+
+    request_string_split_1 = request_string_split_0[1].split("HTTP/1.1")
+
+    url = request_string_split_1[0].strip()
+
+    print(f"Extracted URL: {url} from request!")
+
+    return url
+
 
 
 if __name__ == "__main__":
     main()
+
